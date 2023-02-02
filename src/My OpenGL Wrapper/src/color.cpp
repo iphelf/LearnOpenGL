8e779b86baaa4556e0ddd31c90db4ae73d17894a
Lingfeng Fu <2871618714@qq.com>
2023-02-02 23:05:43 +0800

    remove obsolete static member Colors

--- src/My OpenGL Wrapper/src/color.cpp
+++ src/My OpenGL Wrapper/src/color.cpp
@@ -4,12 +4,6 @@
 
 namespace iphelf::opengl {
 
-const Color Color::Black{0.0f, 0.0f, 0.0f};
-const Color Color::White{1.0f, 1.0f, 1.0f};
-const Color Color::DarkGreenBluish{0.2f, 0.3f, 0.3f};
-const Color Color::Orange{1.0f, 0.5f, 0.2f};
-const Color Color::Yellow{1.0f, 0.0f, 1.0f};
-
 Color Color::from_hsv(float hue, float saturation, float value) {
   auto rgb{glm::rgbColor(glm::vec3{hue, saturation, value})};
   return {rgb.r, rgb.g, rgb.b};
